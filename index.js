12213eba86c93bff96ebb6c753d05b41551a8117
kyj <kimyj1592@naver.com>
2023-07-13 17:03:50 +0900

    refresh token 생성

--- index.js
+++ index.js
@@ -7,13 +7,28 @@ const app = express();
 app.use(express.json());
 
 const secretText = "hello secret user!"; // secret-text
+const refreshText = "hello refresh secret user!"; // refresh-text
+
+// db 대신 배열로 저장 원래는 db에 refreshToken을 저장해야 함
+let refreshTokens = [];
 
 app.post('/login', (req, res) => {
   const userName = req.body.userName;
   const user = { name : userName }; // payload
 
   // 토큰 생성하기 sign(payload, secret-text)
-  const accessToken = jwt.sign(user, secretText);
+  const accessToken = jwt.sign(user, secretText, { expiresIn: '30s' });
+
+  // refresh 토큰 생성하기
+  const refreshToken = jwt.sign(user, refreshText, { expiresIn: '1d' });
+
+  refreshTokens.push(refreshToken)
+    
+    // refreshToken을 쿠키에 저장
+    res.cookie('jwt', refreshToken, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24
+    })
 
   // 토큰 클라이언트에게 전송
   res.json({ accessToken : accessToken })
@@ -36,14 +51,14 @@ app.get('/posts', authMiddleware, (req, res) => {
 });
 
 function authMiddleware(req, res, next) {
-  // 토큰을 request headers에서 가져오기 authHeader은 토큰 주소값을 가짐
+  // 토큰을 request headers에서 가져오기, authHeader은 토큰의 주소값을 가짐(토큰 x)
   const authHeader = req.headers['authorization'];
   // Bearer (토큰)
-  // 여기에서의 authHeader는 null 검사용 무조건 해야 함.
+  // 여기에서 authHeader는 null 검사용 무조건 해야 함.
   const token = authHeader && authHeader.split(' ')[1];
   if(token == null) res.sendStatus(401);
   
-  // 토큰이 있으니 유효한 토큰인지 확인
+  // 토큰이 있으니 그게 유효한 토큰인지 확인
   jwt.verify(token, secretText, (err, user) => {
     if(err) return res.sendStatus(403);
     req.user = user;
